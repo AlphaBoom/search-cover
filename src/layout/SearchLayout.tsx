1810d5d107ab105510e2ac66e8895fcfa0041e88
ZhendongWu <rsshinide38@163.com>
2023-05-16 08:21:53 +0800

    修改滑动行为及样式

--- src/layout/SearchLayout.tsx
+++ src/layout/SearchLayout.tsx
@@ -25,7 +25,7 @@ export default function SearchLayout() {
     let [isLoading, setLoading] = useState(false)
     let [hasMore, setHasMore] = useState(true)
     let [searchResult, setSearchResult] = useState<SearchResult[]>([])
-    var storeRef = useRef<Store>({ session: null, result:[], page: 1 })
+    var storeRef = useRef<Store>({ session: null, result: [], page: 1 })
     const viewChangeListener = (type: ViewType) => {
         setViewMode(type)
     };
@@ -34,12 +34,12 @@ export default function SearchLayout() {
         setFilterCategory(value)
         updateResult()
     }
-    const updateResult = ()=>{
+    const updateResult = () => {
         let store = storeRef.current
-        if (filterCategory == Category.ALL){
+        if (filterCategory == Category.ALL) {
             setSearchResult(store.result)
-        } else if (store.result.length > 0){
-            setSearchResult(store.result.filter((item)=>item.category==filterCategory))
+        } else if (store.result.length > 0) {
+            setSearchResult(store.result.filter((item) => item.category == filterCategory))
         } else {
             setSearchResult([])
         }
@@ -59,14 +59,15 @@ export default function SearchLayout() {
         }
     };
     return (
-        <div className="max-w-5xl w-screen bg-slate-50 grow mx-auto p-3">
+        <div className="max-w-5xl w-screen bg-slate-50 mx-auto p-3 h-screen flex flex-col">
             <SearchBar onKeyDown={onKeyDown} />
-            <div className="flex space-x-4 mt-3">
+            <div className="flex space-x-4 mt-3 grow h-0">
                 <SearchFilterLayout filterOptions={categoryOptions} filterCategory={filterCategory} onSelectedChange={filterCategoryChangeListener} />
-                <div className="flex-auto relative">
+                <div id='SearchResultContainer' className="flex-auto relative overflow-y-auto">
                     {
-                        isLoading ? ((<div className="text-center my-auto absolute left-1/2 top-1/2">加载中...</div>)) :
+                        isLoading ? ((<div className="text-center absolute left-1/2 top-1/2">加载中...</div>)) :
                             searchResult.length > 0 ? (<InfiniteScroll
+                                scrollableTarget='SearchResultContainer'
                                 dataLength={searchResult.length}
                                 next={async () => {
                                     let store = storeRef.current
